f7ee6831a91b6a7ff1ee98fd4d8a40445892c185
Hoang Thai <hoangthai7598@gmail.com>
2022-05-24 23:31:39 +0700

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const userRouter = require('./routers/users')
 
 const proofRouter = require('./routers/proof')
@@ -8,35 +9,37 @@ const upload = require('express-fileupload')
 require('./db/db')
 
 const app = express()
-
+app.use(cors({
+    credentials: true,
+}));
 app.use(express.json())
 
 app.use([userRouter, proofRouter])
 app.use(upload())
 //cors
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-app.post("/upload/file", (req,res) => {
-    if(req.files){
+app.post("/upload/file", (req, res) => {
+    if (req.files) {
         let file = req.files.file;
         let fileName = file.name;
         let uploadPath = __dirname + "./uploads/" + fileName;
 
         file.mv(uploadPath, (err) => {
-            if(err){
+            if (err) {
                 res.send(err);
-            }else {
+            } else {
                 let dataFile = {
                     name: fileName,
-                    size:  file.size,
+                    size: file.size,
                     path: uploadPath
                 }
                 res.json({
-                    message :"File uploaded",
+                    message: "File uploaded",
                     dataFile: dataFile
                 })
             }
